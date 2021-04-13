c743ae2924a71990e2ae3d40715fff33e68321f8
nilesh576 <nileshsingh586@gmail.com>
2021-04-13 17:52:40 +0530

    project upload to github

--- app/src/main/java/com/example/goldenratio/MainActivity.java
+++ app/src/main/java/com/example/goldenratio/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.goldenratio;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,20 +44,22 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-public class MainActivity extends AppCompatActivity implements DailogAddNewChapter.DialogListner {
-    private MenuItem item;
+public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private Button btn, btn2;
+    private Button btn;
     static private TextView textView;
     ArrayList<ChapterCardsDetails> arr;
     static ChapterRecViewAdapter adapter;
     DataBaseHelper db;
     int back;
+    Context contextMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contextMain = MainActivity.this;
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE );
         // very very important
@@ -62,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
         StrictMode.setVmPolicy(builder.build());        
         
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE }, PackageManager.PERMISSION_GRANTED);
+
 
         Intent intent = getIntent();
         String action = intent.getAction();
@@ -95,20 +101,15 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
                 while ((aDataRow = myReader.readLine()) != null) {
                     aBuffer+= aDataRow + "\n";
                 }
+                myReader.close();
 
                 String unHashed = hashTomsg(aBuffer,"questionShared");
                 String[] data = unHashed.split("---new---");
                 String chapter_name = unHashed.split("---new---")[0];
-                DataBaseHelper db = new DataBaseHelper(MainActivity.this);
+                DataBaseHelper db = new DataBaseHelper(contextMain);
 
-                try {
-
-                    db.AddTable(chapter_name,MainActivity.this);
-                }catch (Exception e){
-                    Toast.makeText(this, chapter_name+" already exits, rename or delete exitsing chapter", Toast.LENGTH_SHORT).show();
-                    db.close();
-                    return;
-                }
+                db.AddTable(chapter_name,contextMain);
+                Toast.makeText(this, chapter_name+" already exits, rename or delete exitsing chapter", Toast.LENGTH_SHORT).show();
 
                 String[] question_info;
                 QuestionModel questionModel;
@@ -120,7 +121,6 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
                 db.close();
                 adapter.notifyDataSetChanged();
                 Toast.makeText(this, chapter_name+" chpater has been added to the book \n if already exitsted question would get mergerd", Toast.LENGTH_SHORT).show();
-                myReader.close();
             }
             catch (Exception e) {
                 Toast.makeText(getBaseContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
@@ -151,36 +151,27 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Dialog dialog = new Dialog(contextMain);
+                dialog.setContentView(R.layout.layout_dialog);
+                EditText dialog_editText_chap_name =  dialog.findViewById(R.id.dailog_chapter_name);
+                Button yesbutton = dialog.findViewById(R.id.add_chapter);
 
-                openDialog();
+                yesbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String chapter_name_to_be_add = dialog_editText_chap_name.getText().toString();
+                        DataBaseHelper db = new DataBaseHelper(contextMain);
+                        db.AddTable(chapter_name_to_be_add,contextMain);
+                        db.close();
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
 
     }
 
-
-
-
-    public void openDialog() {
-        DailogAddNewChapter dailogAddNewChapter = new DailogAddNewChapter();
-        dailogAddNewChapter.show(getSupportFragmentManager(), "example dailog");
-
-    }
-
-    @Override
-    public void applychanges(String questionName) {
-
-        db.AddTable(questionName,MainActivity.this);
-
-        arr = db.is_there_table();
-        adapter.setCards(arr);
-        adapter.notifyDataSetChanged();
-        if (arr.isEmpty()) {
-            textView.setVisibility(View.VISIBLE);
-        } else {
-            textView.setVisibility(View.GONE);
-        }
-    }
 
 
     static public void change(ArrayList<ChapterCardsDetails>arr){
@@ -190,14 +181,14 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
             textView.setVisibility(View.GONE);
         }
         adapter.setCards(arr);
-        adapter.notifyDataSetChanged();
+//        adapter.notifyDataSetChanged();
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode== KeyEvent.KEYCODE_BACK){
             if (back == 0) {
-                CountDownTimer countDownTimer = new CountDownTimer(1000,200) {
+                CountDownTimer countDownTimer = new CountDownTimer(1000,1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
                         back = 1;
@@ -210,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
 
                 };
                 countDownTimer.start();
-                Toast.makeText(MainActivity.this,"press again to go back",Toast.LENGTH_SHORT).show();
+                Toast.makeText(contextMain,"press again to go back",Toast.LENGTH_SHORT).show();
                 return true;
             }
             else{
@@ -222,34 +213,8 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
         return super.onKeyDown(keyCode, event);
     }
 
-    public void open_on_start() {
-        Intent i = getIntent();
-        Uri data = i.getData();
-        if (data == null)
-        {
-            return;
-        }
-        URL url;
-        String startFile = "";
-        try
-        {
-            url = new URL(data.getScheme(), data.getHost(), data.getPath());
-            startFile = url.toString().replace("file:", "");
-        }
-        catch (Exception ex)
-        {
-            Toast.makeText(this, "Error:\n" + ex.getMessage().toString(), Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (startFile == null)
-        {
-            return;
-        }
-        StringBuilder text = new StringBuilder();
-        boolean can = false;
-        boolean sel = false;
-    }
-    private String hashTomsg(String hash, String key) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+    private String hashTomsg(String hash, String key) throws Exception{
         if(hash==null){
             hash = "";
         }
@@ -264,6 +229,7 @@ public class MainActivity extends AppCompatActivity implements DailogAddNewChapt
         String decrypted_msg = new String(decValue);
         return decrypted_msg;
     }
+
     private SecretKeySpec generateKey(String passwaord) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         byte[] bytes = passwaord.getBytes("UTF-8");
